4a5b0d2f65de51b51f32addefd66f4a6d1fd88e6
Bulla Kolia <koliakolan@gmail.com>
2022-09-25 19:45:48 +0300

    семинар и дз

---
+++ dz/Program.cs
@@ -0,0 +1,200 @@
+﻿int [,] CreateArrayMatrix (int m, int n, int x, int y){ // m - rom; n - column;  x - min element; y - max+1 element
+    int [,] ar = new int [m,n];
+    for (int i = 0; i < ar.GetLength(0); i++)
+    {
+        for (int j = 0; j < ar.GetLength(1); j++)
+        {
+            ar [i,j] = new Random().Next(x, y);
+        }
+    }
+    return ar;
+}
+
+int [] ShellSort(int []ar)
+{
+    int i, j, step;
+    int help;
+    for (step = ar.Length / 2; step > 0; step /= 2){
+        for (i = step; i < ar.Length; i++)
+        {
+            help = ar[i];
+            for (j = i; j >= step; j -= step)
+            {
+                if (help < ar[j - step])
+                    ar[j] = ar[j - step];
+                else
+                    break;
+            }
+            ar[j] = help;
+        }
+    }
+    return ar;
+}
+
+void PrintArrayMatrix (int[,] ar){
+    for (int i = 0; i < ar.GetLength(0); i++)
+    {
+        for (int j = 0; j < ar.GetLength(1); j++){
+            Console.Write($"{ar[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int MinElementArrayIndex (int[] ar){
+    int min = ar[0];
+    int index = 0;
+    for (int i = 0; i < ar.Length; i++)
+    {
+        if (min > ar[i]){
+            min = ar[i];
+            index = i;
+        }
+    }
+    return index;
+}
+/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы 
+каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2*/
+/*
+int [,] array54 = CreateArrayMatrix(4, 6, -10, 100);
+PrintArrayMatrix(array54);
+int [] array54help = new int [array54.GetLength(1)];
+for (int i = 0; i < array54.GetLength(0); i++)
+{
+    for (int j = 0; j < array54.GetLength(1); j++)
+    {
+        array54help[j] = array54[i,j];
+    }
+    ShellSort(array54help);
+    for (int j = 0; j < array54.GetLength(1); j++)
+    {
+        array54[i,j] = array54help[j];
+    }
+}
+PrintArrayMatrix(array54);
+*/
+
+
+
+/*Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки 
+с наименьшей суммой элементов: 1 строка*/
+
+/*
+int [,] array56 = CreateArrayMatrix(3, 3, -10, 100);
+PrintArrayMatrix(array56);
+int [] sumrows = new int [array56.GetLength(0)];
+for (int i = 0; i < array56.GetLength(0); i++)
+{   
+    int sumrow = 0;
+    for (int j = 0; j < array56.GetLength(1); j++)
+    {
+        sumrows[i] += array56[i,j];
+    }
+}
+Console.WriteLine($"Номер строки с наименьшей суммой элементов: {MinElementArrayIndex (sumrows)+1} строка");
+*/
+
+/*Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18*/
+/*
+void MatrixMultiplication (int [,] matrixmultiplication, int [,] ar1, int [,] ar2){
+    if (ar1.GetLength(0) != ar2.GetLength(1)){
+         Console.WriteLine("Умножение матриц не возможно!");
+    }
+    else{
+        for (int i = 0; i < ar1.GetLength(0); i++)
+        {
+            for (int j = 0; j < ar2.GetLength(1); j++)
+            {
+                for (int k = 0; k < ar2.GetLength(0); k++)
+                {
+                    matrixmultiplication[i,j] += ar1[i,k] * ar2[k,j];
+                }
+            }
+        }
+    }
+}
+
+int [,] array581 = CreateArrayMatrix(2, 3, 0, 6);
+PrintArrayMatrix(array581);
+
+int [,] array582 = CreateArrayMatrix(3, 2, 0, 6);
+PrintArrayMatrix(array582);
+
+int [,] array581582 = new int [array581.GetLength(0), array582.GetLength(1) ];
+MatrixMultiplication(array581582, array581, array582);
+PrintArrayMatrix(array581582);
+*/
+
+
+/*Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)*/
+/*
+int [,,] array60 = new int [2,2,2];
+int q = 1;
+for (int i = 0; i < array60.GetLength(0); i++)
+{
+    for (int j = 0; j < array60.GetLength(1); j++)
+    {
+        for (int k = 0; k < array60.GetLength(2); k++)
+        {
+            Console.Write($"{array60[i,j,k]=q} : [{i},{j},{k}] ");
+            q++;
+        }
+        Console.WriteLine();
+    }
+}*/
+    
+
+
+/*Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07*/
+
+
+int [,] array62 = new int [4,4];
+    for (int i = 0; i < array62.GetLength(0)/2; i++)
+    {
+        for (int j = i; j < array62.GetLength(1)-1-i; j++)
+        {
+            array62 [i,j] = 10*i+i+j+1;
+            array62 [j, array62.GetLength(1)-1-i] = array62 [i,j] + array62.GetLength(1) - 2*i - 1; 
+            array62 [array62.GetLength(0)-1-i, array62.GetLength(1)-1-j] 
+                        = array62 [j, array62.GetLength(1)-1-i]+array62.GetLength(0)-2*i-1;
+            array62 [array62.GetLength(1)-j-1, i] 
+                        = array62 [array62.GetLength(0)-i-1, array62.GetLength(1)-j-1] + array62.GetLength(1)-2*i-1;
+            Console.WriteLine(i);
+        }
+        
+    }
+
+    PrintArrayMatrix(array62);
---
+++ work/Program.cs
@@ -0,0 +1,313 @@
+﻿int [] CreateArray (int n, int x, int y){ // n - size;  x - min element; y - max+1 element
+    int [] array = new int [n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = new Random().Next(x, y);
+    }
+    return array;
+}
+
+void PrintArray (int[] ar){
+    for (int i = 0; i < ar.Length; i++)
+    {
+        Console.Write($"{ar[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int MaxElementArray (int[] ar){
+    int max = ar[0];
+    for (int i = 0; i < ar.Length; i++)
+    {
+        if (max < ar[i]){
+            max = ar[i];
+        }
+    }
+    return max;
+}
+
+int MinElementArray (int[] ar){
+    int min = ar[0];
+    for (int i = 0; i < ar.Length; i++)
+    {
+        if (min > ar[i]){
+            min = ar[i];
+        }
+    }
+    return min;
+}
+
+int [] ShellSort(int []ar)
+{
+    int i, j, step;
+    int help;
+    for (step = ar.Length / 2; step > 0; step /= 2){
+        for (i = step; i < ar.Length; i++)
+        {
+            help = ar[i];
+            for (j = i; j >= step; j -= step)
+            {
+                if (help < ar[j - step])
+                    ar[j] = ar[j - step];
+                else
+                    break;
+            }
+            ar[j] = help;
+        }
+    }
+    return ar;
+}
+
+//------------Двумерные массивы--------------//
+
+int [,] CreateArrayMatrix (int m, int n, int x, int y){ // m - rom; n - column;  x - min element; y - max+1 element
+    int [,] ar = new int [m,n];
+    for (int i = 0; i < ar.GetLength(0); i++)
+    {
+        for (int j = 0; j < ar.GetLength(1); j++)
+        {
+            ar [i,j] = new Random().Next(x, y);
+        }
+    }
+    return ar;
+}
+
+void PrintArrayMatrix (int[,] ar){
+    for (int i = 0; i < ar.GetLength(0); i++)
+    {
+        for (int j = 0; j < ar.GetLength(1); j++){
+            Console.Write($"{ar[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int MaxElementArrayMatrix (int[,] ar){
+    int max = ar[0,0];
+    for (int i = 0; i < ar.GetLength(0); i++)
+    {
+        for (int j = 0; j < ar.GetLength(1); j++)
+        {
+           if (max < ar[i,j]){
+                max = ar[i,j];
+            } 
+        }
+            
+    }
+    return max;
+}
+
+int MinElementArrayMatrix (int[,] ar){
+    int min = ar[0,0];
+    for (int i = 0; i < ar.GetLength(0); i++)
+    {
+        for (int j = 0; j < ar.GetLength(1); j++)
+        {
+           if (min > ar[i,j]){
+                min = ar[i,j];
+            } 
+        }
+            
+    }
+    return min;
+}
+
+int [] MatrixToArray(int [,]ar){
+    int [] array = new int [ar.GetLength(0) * ar.GetLength(1)];
+    for (int i = 0, z = 0; i < ar.GetLength(0); i++)
+    {
+        for (int j = 0; j < ar.GetLength(1); j++, z++)
+        {
+            array[z] = ar[i,j];
+        }
+    }
+    return array;
+}
+
+int [,] ArrayToMatrix(int []ar, int m, int n){
+    int [,] array = new int [m, n];
+    for (int i = 0, z = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++, z++)
+        {
+            array[i,j] = ar[z];
+        }
+    }
+    return array;
+}
+
+void ChangeRowsAndColumns(int [,] ar){
+    int help = 0;
+    if(ar.GetLength(0) == ar.GetLength(0)){
+        for (int i = 0; i < ar.GetLength(0); i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                help = ar[i,j];
+                ar[i,j] = ar[j,i];
+                ar[j,i] = help;
+                //Console.Write($"{ar[j,i]} ");
+            }
+        }
+    }
+}
+
+int [,] TrianglePascals (int [,] ar, int n){ // n - column, rows;  
+    ar [0, (n+n/2)] = 1;
+    ar [1, (n+n/2)-1] = 1;
+    ar [1, (n+n/2)+1] = 1;
+    for (int i = 2; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {   if(i % 2 == 0){
+                ar [i, (n+n/2)] =         ar[i-1, (n+n/2 - 1)] + ar[i-1, (n + n/2 + 1)];
+                ar [i, (n+n/2) + (j+2)] = ar[i-1, ((n+n/2) + (j+2)-1)] + ar[i-1, ((n+n/2) + (j+2)+1)];
+                ar [i, (n+n/2) - (j+2)] = ar[i-1, ((n+n/2) - (j+2)-1)] + ar[i-1, ((n+n/2) - (j+2)+1)];
+            }
+            else {
+                ar [i, (n+n/2) + 1 + j] = ar[i-1, (n+n/2) + j] + ar[i-1, (n+n/2) + 2 + j];
+                ar [i, (n+n/2) - 1 - j] = ar[i-1, (n+n/2) - j] + ar[i-1, (n+n/2) - 2 - j];
+            }
+        }
+    }
+    return ar;
+}
+
+
+/*Задача 53: Задайте двумерный массив. Напишите программу, 
+которая поменяет местами первую и последнюю строку массива.*/
+/*
+void ChangeFirstAndLastRow (int [,] ar){
+    int help = 0;
+    for (int i = 0; i < ar.GetLength(1); i++)
+    {
+        help = ar[0,i];
+        ar[0,i] = ar[ar.GetLength(0) - 1 , i];
+        ar[ar.GetLength(0) - 1 , i] = help;
+    }
+}
+
+Console.WriteLine("Задача 53");
+int [,] array53 = CreateArrayMatrix(4, 3, 0, 10);
+PrintArrayMatrix(array53);
+ChangeFirstAndLastRow(array53);
+PrintArrayMatrix(array53);
+*/
+
+/*Задача 55: Задайте двумерный массив. Напишите программу, 
+которая заменяет строки на столбцы. В случае, если это невозможно, 
+программа должна вывести сообщение для пользователя.*/
+
+
+    
+/*
+Console.WriteLine("Задача 53");
+int [,] array55 = CreateArrayMatrix(5, 5, 0, 10);
+PrintArrayMatrix(array55);
+ChangeRowsAndColumns(array55);
+PrintArrayMatrix(array55);
+
+
+*/
+/*Задача 57: Составить частотный словарь элементов двумерного массива. Частотный словарь 
+содержит информацию о том, сколько раз встречается элемент входных данных.*/
+/*
+void FrequencyDictionary(int [] ar){
+    
+    for (int i = 0; i < ar.Length; i++)
+    {
+        int count = 1;
+        for (int j = i; j < ar.Length; j++){
+            if(j != ar.Length-1 && ar[j] == ar[j+1]){
+                count++;
+            }
+            else {
+                i=j;
+                break;
+            }
+        }
+        Console.WriteLine($"Число {ar[i]} встречается {count} раз");
+    }
+}
+
+Console.WriteLine("Задача 57");
+int [,] array57 = CreateArrayMatrix(5, 4, 0, 10);
+PrintArrayMatrix(array57);
+int [] array57row = ShellSort(MatrixToArray(array57)); // перевожу двумерный массив в одномерный
+                                                        // и сортирую по возрастанию
+FrequencyDictionary(array57row);
+
+*/
+/*Задача 59: Задайте двумерный массив из целых чисел. Напишите программу, 
+которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Наименьший элемент - 1, на выходе получим 
+следующий массив:
+9 4 2
+2 2 6
+3 4 7*/
+/*
+Console.WriteLine("Задача 59");
+int [,] array59 = CreateArrayMatrix(5, 4, -100, 1000);
+PrintArrayMatrix(array59);
+
+int [] minPosition = new int [2]; // массив с коорд мин позиции
+
+int min = array59[0,0];
+for (int i = 0; i < array59.GetLength(0); i++)  // ищем минимальный элемент и запоминаем позицию
+    {
+        for (int j = 0; j < array59.GetLength(1); j++)
+        {
+           if (min > array59[i,j]){
+                min = array59[i,j];
+                minPosition [0] = i;
+                minPosition [1] = j;
+
+            } 
+        }
+            
+    }
+
+    Console.WriteLine($"Мин = {min} ");
+    Console.WriteLine(minPosition[0]);
+    Console.WriteLine(minPosition[1]);
+    
+int [,] array59help = new int [array59.GetLength(0)-1, array59.GetLength(1)-1];
+for (int i = 0; i < array59.GetLength(0)-1; i++)  //в этом цикле мы копируем в новый массив элементы старого массива
+    {                                               // без строки и столбца с мин элементом
+        for (int j = 0; j < array59.GetLength(1)-1; j++)
+        {
+            if (i < minPosition[0] && j < minPosition[1] ) { 
+                array59help[i, j] = array59[i, j];
+            }
+            else if (i < minPosition[0] && j >= minPosition[1]){
+                    array59help[i, j] = array59[i, j+1];
+            }
+            else if (i >= minPosition[0] && j < minPosition[1]){
+                    array59help[i, j] = array59[i+1, j];
+            }
+            else if (i >= minPosition[0] && j >= minPosition[1]){
+                array59help[i, j] = array59[i+1, j+1];
+            }
+        }  
+    }
+
+PrintArrayMatrix(array59help);
+
+*/
+
+/*Задача 61: Вывести первые N строк треугольника Паскаля. 
+Сделать вывод в виде равнобедренного треугольника*/
+
+Console.WriteLine("Задача 61");
+Console.WriteLine("Введите количество строк");
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] array61 = new int [n, 3*n];
+
+
+PrintArrayMatrix(TrianglePascals (array61, n));
